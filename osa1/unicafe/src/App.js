76290917adbe4e9434f143d96dc8dbc5c359f999
Lauri Salminen <salminen.lauri.a@gmail.com>
2023-02-12 18:25:34 +0200

    1.8, 1.9 ja 1.10

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -9,18 +9,46 @@ const Button =({handleClick, text}) => (
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.name}</h1>
+      <h1>{props.text}</h1>
     </div>
   )
 }
 
-const Counter = (props) => {
+const StatisticLine = (props) => {
   return (
     <div>
-      <p>{props.name} {props.clicks} {props.unit}</p>
+      <p>{props.text} {props.value} {props.unit}</p>
     </div>
   )
   }
+
+const Statistics = (props) => {
+  const good = props.good
+  const bad = props.bad
+  const neutral = props.neutral 
+  const total = good+bad+neutral
+  const average = (good * 1 + bad * -1) / total
+  const positive = good * (100/total)
+  
+  if (total===0){
+    return(
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good}/>
+      <StatisticLine text="neutral" value={neutral}/>
+      <StatisticLine text="bad" value={bad}/>
+      <StatisticLine text="total" value={total}/>        
+      <StatisticLine text="average" value={average}/>
+      <StatisticLine text="positive" value={positive} unit="%"/>
+    </div>
+  )
+}
 
 
 
@@ -29,9 +57,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good+bad+neutral
-  const average = (good * 1 + bad * -1) / total
-  const positive = good * (100/total)
 
   const handleGoodClick = () => {
     setGood(good+1)
@@ -47,21 +72,14 @@ const App = () => {
 
   return (
     <div>
-      <Header name="give feedback"/>
+      <Header text="give feedback"/>
       <div>
         <Button handleClick={handleGoodClick} text='good'/>
         <Button handleClick={handleNeutralClick} text='neutral'/>
         <Button handleClick={handleBadClick} text='bad'/>
       </div>
-      <Header name="statistics"/>
-      <div>
-        <Counter name="good" clicks={good}/>
-        <Counter name="neutral" clicks={neutral}/>
-        <Counter name="bad" clicks={bad}/>
-        <Counter name="total" clicks={total}/>
-        <Counter name="average" clicks={average}/>
-        <Counter name="positive" clicks={positive} unit="%"/>
-      </div>
+      <Header text="statistics"/>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
