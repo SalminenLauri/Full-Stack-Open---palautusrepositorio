34f61c3a108539815c0b35e911e985f43dc8c841
Lauri Salminen <salminen.lauri.a@gmail.com>
2023-02-19 15:52:24 +0200

    2.5.

--- osa3/kurssitiedot/src/App.js
+++ osa3/kurssitiedot/src/App.js
@@ -1,40 +1,5 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
+import Course from './components/Course'
 
-const Total = ({parts}) => {
-  const total = 
-    parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <p><b>Number of exercises: {total}</b></p>
-}
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} part={part}/>
-      )}
-    </div>
-  )
-}
 
 const App = () => {
   const courses = [
